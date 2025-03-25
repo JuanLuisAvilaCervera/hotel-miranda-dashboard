b78bba6b3baa9a67db4166ab92bd3042e5d5b471
JuanLuisAvilaCervera <juanluisavilacervera44@gmail.com>
2025-03-25 13:00:26 +0100

    react-router terminado

---
+++ src/main.jsx
@@ -0,0 +1,34 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import { Provider } from 'react-redux'
+import Store from './app/store'
+
+import DashboardPage from './page/DashboardPage';
+import LoginPage from './page/LoginPage'
+import Layout from './components/Layout/Layout'
+import PrivateRoute from './components/Login/PrivateRoot'
+import RoomPage from './page/RoomPage'
+import BookingsPage from './page/BookingsPage'
+import ContactsPage from './page/ContactsPage'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Provider store={Store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login' element={<LoginPage />} />
+          <Route element={<Layout/>}>
+            <Route element={<PrivateRoute />} >
+                <Route path="/" element={<DashboardPage />}/>
+                <Route path="/rooms" element={<RoomPage />}/>
+                <Route path='/bookings' element={<BookingsPage />}/>
+                <Route path='/contacts' element={<ContactsPage />}/>
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>,
+)
---
+++ src/page/RoomPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const RoomPage = () => {
+
+    return <>
+            <h1>Rooms Page</h1>
+        </>;
+}
+
+export default RoomPage;
