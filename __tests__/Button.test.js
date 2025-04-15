70ed11365ac5332023a37b8f32ebc34761c904f3
JuanLuisAvilaCervera <juanluisavilacervera44@gmail.com>
2025-04-15 10:20:17 +0200

    tests finished

--- __tests__/Button.test.js
+++ __tests__/Button.test.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import { render, cleanup } from '@testing-library/react'
-import { Button } from '../src/components/common/Buttons'
+import { Button, InputButton } from '../src/components/common/Buttons'
 
 afterEach(cleanup)
-test('It has to show color #fc634c and backgroundcolor #23f538', () => {
+
+/**
+ * @jest-environment ./jest.config.cjs
+ */
+
+test('It has to show color rgb(252,99,76) and backgroundcolor rgb(35,245,56)', () => {
   render(
     <div>
-      <Button color={'#fc634c'} $backgroundcolor={'#23f538'}/>
+      <Button color={"rgb(252,99,76)"} $backgroundcolor={'rgb(35,245,56)'}/>
     </div>
   )
-  const buttonClass = Button().type.styledComponentId
+  const buttonClass = Button.styledComponentId
   const buttonRoot = document.getElementsByClassName(buttonClass)
   const style = window.getComputedStyle(buttonRoot[0])
-  expect(style.color).toBe('#fc634c')
-  expect(style.$backgroundcolor).toBe('#23f538')
+  expect(style.color).toBe("rgb(252, 99, 76)")
+  expect(style.backgroundColor).toBe('rgb(35, 245, 56)')
 })
