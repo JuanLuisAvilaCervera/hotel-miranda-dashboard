885b385bb13120fd52b445b7c760bb9be18835a8
JuanLuisAvilaCervera <juanluisavilacervera44@gmail.com>
2025-04-24 20:34:17 +0200

    mejorando

--- src/page/Bookings/BookingsList.tsx
+++ src/page/Bookings/BookingsList.tsx
@@ -36,8 +36,10 @@ const BookingsPage = () => {
     }, [dispatch , bookingsStatus , bookingsData])
 
 
-    const filterBooking = (listFilter : string) => bookingList.filter((booking : Booking) => booking.status === listFilter || []);
-        const changeActive = (listName : string) => {
+    const filterBooking = (listFilter : string) => { return bookingsData.filter((booking : Booking) => booking.status === listFilter ) || [] }
+
+
+    const changeActive = (listName : string) => {
         setActive(listName);
             switch(listName){
                 case "all":
@@ -78,6 +80,8 @@ const BookingsPage = () => {
         }));
     }
 
+   
+
     useEffect(() => { handleOrder()}, [order, active])
 
 
@@ -107,3 +111,79 @@ const BookingsPage = () => {
 }
 
 export default BookingsPage;
+
+
+
+// const BookingsPage = () => {
+
+
+//     const changeActive = (listName) => {
+//         setActive(listName);
+//             switch(listName){
+//                 case "all":
+//                     setData(bookingsData);
+//                     break;
+//                 case "checkin":
+//                     setData(filterBooking("Check In"));
+//                     break;
+//                 case "checkout":
+//                     setData(filterBooking("Check Out"))
+//                     break;
+//                 case "progress":
+//                     setData(filterBooking("In Progress"));
+//                     break;
+//                 default:
+//                     setData(users);
+//                     break;
+//             }
+        
+//     }
+
+//     const handleOrder = () => {
+//         setData([...data].sort((a,b) => {
+
+//             switch(order){
+//                 case "order_date":
+//                     return new Date(b.order_date) - new Date(a.order_date);
+//                 case "check_in_date":
+//                     return new Date(b.check_in_date) - new Date(a.check_in_date);
+//                 case "check_out_date":
+//                     return new Date(b.check_out_date) - new Date(a.check_out_date);
+//                 case "guest":
+//                     return a.client_id - b.client_id; // CHANGE
+//                 default:
+//                     return 0;
+//             }
+//         }));
+//     }
+
+//     useEffect(() => { handleOrder()}, [order, active])
+
+
+
+//     return <Page>
+//             <TableNav $justify={"space-between"}>
+//             <UnorderedList>
+//                 <NavList $active={active === "all" ? "active" : ""}  onClick = {  () =>changeActive("all")}>All Bookings</NavList>
+//                 <NavList $active={active === "checkin" ? "active" : ""} onClick = {  () => changeActive("checkin")}>Checking In</NavList>
+//                 <NavList $active={active === "checkout" ? "active" : ""}  onClick = {  () => changeActive("checkout")}>Checking Out</NavList>
+//                 <NavList $active={active === "progress" ? "active" : ""} onClick = {  () => changeActive("progress")}>In Progress</NavList>
+//             </UnorderedList>
+//             <Button onClick={ () => navigate("/newbookings")}>Add Booking</Button>
+//             <OrderSelectDiv>
+//                     <OrderSelect
+//                 value={order}
+//                 onChange={(e) => {setOrder(e.target.value)}}>
+//                 <option value="order_date"> Newest </option>
+//                 <option value="check_in_date"> Check In </option>
+//                 <option value="check_out_date"> Check Out </option>
+//                 <option value="guest"> Guest </option>
+//                 </OrderSelect>
+//             </OrderSelectDiv>
+//             </TableNav>
+            
+//             {data.length > 0 || bookingsStatus === "fulfilled" ? data.length > 0 ? <Table data={data} dataType={"bookings"}/> : <h1>No bookings found</h1> : <h1>Loading...</h1>}
+//             </Page>;
+// }
+
+// export default BookingsPage;
